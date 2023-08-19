d3d72ad8e824d759832e9a78705816780fd153cb
Nevindu12 <nilangithnevindu@gmail.com>
2023-08-19 07:48:53 +0530

    add navbarOne

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
+import {SiConsul} from 'react-icons/si'
+import {BsPhoneVibrate} from 'react-icons/si'
+import {AiOutlineGlobal} from 'react-icons/si'
+
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='navBar flex'>
+            <div className="navBarOne flex">
+                <div>
+                    <SiConsul/>
+                </div>
+
+                <div className='none flex'>
+                    <li className='flex'> <BsPhoneVibrate/> Support</li>
+                    <li className='flex'> <AiOutlineGlobal/> Languages</li>
+                </div>
+
+                <div className="atb flex">
+                    <span>Sign IN</span>
+                    <span>Sign OUT</span>
+                </div>
+
+
+            </div>
+        </div>
     )
 }
 
-export default Navbar
+export default Navbar 
