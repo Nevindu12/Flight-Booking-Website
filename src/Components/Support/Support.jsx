6da583b313d07ab94457ed94912a02561260bef3
Nevindu12 <nilangithnevindu@gmail.com>
2023-08-28 21:48:36 +0530

    add support page content

--- src/Components/Support/Support.jsx
+++ src/Components/Support/Support.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import gridImage from '../../assets/images-grid.png'
 
 const Support = () => {
     return (
-        <div>Support</div>
+        <div className='support container section'>
+            <div className="sectionContainer">
+                <div className="titlesDiv">
+                    <small>travel support</small>
+                    <h2>Plane your travel with confidence</h2>
+                    <p>Find help with booking and travel plans, see what to expect along the journey!</p>
+                </div>
+
+                <div className="infoDiv grid">
+                    <div className="textDiv grid">
+
+                        <div className="singleInfo">
+                            <span className='number'>01</span>
+                            <h4>Travel requirements for Dubai</h4>
+                            <p>Find help with booking and travel plans, see what to expect along the journey to your favourite destinations!</p>
+                        </div>
+
+                        <div className="singleInfo">
+                            <span className='number'>02</span>
+                            <h4>Chauffeur services at your arrival</h4>
+                            <p>Find help with booking and travel plans, see what to expect along the journey to your favourite destinations!</p>
+                        </div>
+
+                        <div className="singleInfo">
+                            <span className='number'>03</span>
+                            <h4>Multi-risk travel insurance</h4>
+                            <p>Find help with booking and travel plans, see what to expect along the journey to your favourite destinations!</p>
+                        </div>
+
+                    </div>
+
+                    <div className="imgDiv">
+                        <img src={gridImage}/>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
