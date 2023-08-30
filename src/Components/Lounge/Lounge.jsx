22ecc79c600ce1976629887def4af108d8df3df9
Nevindu12 <nilangithnevindu@gmail.com>
2023-08-30 23:30:37 +0530

    add lounge section content styles

--- src/Components/Lounge/Lounge.jsx
+++ src/Components/Lounge/Lounge.jsx
@@ -13,30 +13,29 @@ const Lounge = () => {
 
                 <div className="textDiv">
                     <h2>Unaccompanied Minor Lounge</h2>
-                </div>
+                    <div className="grids grid">
 
-                <div className="grids grid">
+                        <div className="singleGrid">
+                            <span className='gridTitle'>Help through the Airport</span>
+                            <p>You can also call airlines from your phone and book a flight ticket to one of your favorite destinations</p>
+                        </div>
 
-                    <div className="singleGrid">
-                        <span className='gridTitle'>Help through the airport</span>
-                        <p>You can also call airlines from your phone and book a flight ticket to one of your favorite destinations</p>
+                        <div className="singleGrid">
+                            <span className='gridTitle'>Priority Boarding</span>
+                            <p>You can also call airlines from your phone and book a flight ticket to one of your favorite destinations</p>
+                        </div>
+
+                        <div className="singleGrid">
+                            <span className='gridTitle'>Care on the Flight</span>
+                            <p>You can also call airlines from your phone and book a flight ticket to one of your favorite destinations</p>
+                        </div>
+
+                        <div className="singleGrid">
+                            <span className='gridTitle'>Chauffeur-drive Service</span>
+                            <p>You can also call airlines from your phone and book a flight ticket to one of your favorite destinations</p>
+                        </div>
+
                     </div>
-
-                    <div className="singleGrid">
-                        <span className='gridTitle'>Priority Boarding</span>
-                        <p>You can also call airlines from your phone and book a flight ticket to one of your favorite destinations</p>
-                    </div>
-
-                    <div className="singleGrid">
-                        <span className='gridTitle'>Care on the flight</span>
-                        <p>You can also call airlines from your phone and book a flight ticket to one of your favorite destinations</p>
-                    </div>
-
-                    <div className="singleGrid">
-                        <span className='gridTitle'>Chauffeur-drive service</span>
-                        <p>You can also call airlines from your phone and book a flight ticket to one of your favorite destinations</p>
-                    </div>
-
                 </div>
 
             </div>
