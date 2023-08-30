1630d058c1b963d70521a78c6c2083d8bad71c59
Nevindu12 <nilangithnevindu@gmail.com>
2023-08-30 23:56:29 +0530

    add traveler section & traveler1 content

--- src/Components/Travelers/Travelers.jsx
+++ src/Components/Travelers/Travelers.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
 
+import paris from '../../assets/paris.jpg'
+
+import traveler1 from '../../assets/user1.jpg'
+
 const Travelers = () => {
     return (
-        <div>Travelers</div>
+        <div className='travelers container section'>
+            <div className="sectionContainer">
+                <h2>Top travelers of this month!</h2>
+
+                <div className="travelersContainer grid">
+
+                    <div className="singleTraveler">
+                        <img src={paris} className='destinationImage'/>
+                        <div className="travelerDetails">
+                            <div className="travelersPicture">
+                                <img src={traveler1} className='travelerImage'/>
+                            </div>
+                            <div className="travelerName">
+                                <span>IsraTech</span>@isratech8
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
     )
 }
 
