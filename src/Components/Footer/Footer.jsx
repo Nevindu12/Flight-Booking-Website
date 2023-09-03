c5e4143eb63ff80758c8506f178213fa14ba256f
Nevindu12 <nilangithnevindu@gmail.com>
2023-09-03 19:11:57 +0530

    add footer section & content

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,101 @@
 import React from 'react'
 
+import Logo from '../../assets/logo.png'
+
+import {TiSocialFacebook} from 'react-icons/ti'
+import {AiOutlineTwitter} from 'react-icons/ai'
+import {AiFillYoutube} from 'react-icons/ai'
+import {FaPinterestP} from 'react-icons/fa'
+
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className='footer'>
+            <div className="sectionContainer container grid">
+                <div className="gridOne">
+                    <div className="logoDiv">
+                        <img src={Logo} className='Logo' />
+                    </div>
+                    <p>Yor mind should be strong than your feelings, fly!</p>
+                    <div className="socialIcon flex">
+                        <TiSocialFacebook className='icon'/>
+                        <AiOutlineTwitter className='icon'/>
+                        <AiFillYoutube className='icon'/>
+                        <FaPinterestP className='icon'/>
+                    </div>
+                </div>
+
+                <div className="footerLinks">
+                    <span className="linkTitle">Information</span>
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="">Explore</a>
+                    </li>
+                    <li>
+                        <a href="">Flight Status</a>
+                    </li>
+                    <li>
+                        <a href="">Travel</a>
+                    </li>
+                    <li>
+                        <a href="">Check-In</a>
+                    </li>
+                    <li>
+                        <a href="">Manage Yor Booking</a>
+                    </li>
+                </div>
+
+                <div className="footerLinks">
+                    <span className="linkTitle">Quick Guide</span>
+                    <li>
+                        <a href="">FAQ</a>
+                    </li>
+                    <li>
+                        <a href="">How to</a>
+                    </li>
+                    <li>
+                        <a href="">Features</a>
+                    </li>
+                    <li>
+                        <a href="">Baggage</a>
+                    </li>
+                    <li>
+                        <a href="">Route Map</a>
+                    </li>
+                    <li>
+                        <a href="">Our Communities</a>
+                    </li>
+                </div>
+
+                <div className="footerLinks">
+                    <span className="linkTitle">Information</span>
+                    <li>
+                        <a href="">Chauffuer</a>
+                    </li>
+                    <li>
+                        <a href="">Our Partners</a>
+                    </li>
+                    <li>
+                        <a href="">Destinations</a>
+                    </li>
+                    <li>
+                        <a href="">Careers</a>
+                    </li>
+                    <li>
+                        <a href="">Transportation</a>
+                    </li>
+                    <li>
+                        <a href="">Programmer Rules</a>
+                    </li>
+                </div>
+            </div>
+
+            <div className="copyRightDiv flex">
+                <p>Customer Design | Developed by <a href="#" target='_blank'>nevindu</a></p>
+            </div>
+            
+        </div>
     )
 }
 
