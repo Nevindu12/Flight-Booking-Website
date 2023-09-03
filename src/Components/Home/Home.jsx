dd438dc90fa64876f280b6b95c78ce6982a6bd5e
Nevindu12 <nilangithnevindu@gmail.com>
2023-09-03 23:56:05 +0530

    add scrolling animation home>info section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 import video from '../../assets/sky.mp4'
 import plane from '../../assets/takeoffplane.png'
 
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
 const Home = () => {
+    //use effect
+    useEffect(()=>{
+        Aos.init({duration: 2000})
+    }, [])
+
     return (
         <div className='home flex container'>
 
             <div className="mainText">
-                <h1>Create Ever-lasting Memories With Us</h1>
+                <h1 data-aos='fade-up' data-aos-duration='2500'>Create Ever-lasting Memories With Us</h1>
             </div>
 
             <div className="homeImages flex">
--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 import { HiOutlineLocationMarker } from 'react-icons/hi'
 import { RiAccountPinCircleLine } from 'react-icons/ri'
 import { RxCalendar } from 'react-icons/rx'
 
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
 const Search = () => {
+    //use effect
+    useEffect(()=>{
+        Aos.init({duration: 2000})
+    }, [])
+
     return (
         <div className='search container section'>
 
-            <div className="sectionContainer grid">
+            <div data-aos='fade-up' data-aos-duration='2500' className="sectionContainer grid">
 
                 <div className="btns flex">
 
@@ -26,7 +34,7 @@ const Search = () => {
 
                 </div>
 
-                <div className="searchInputs flex">
+                <div data-aos='fade-up' data-aos-duration='2000' className="searchInputs flex">
                     {/* single input */}
                     <div className="singleInput flex">
                         <div className="iconDiv">
