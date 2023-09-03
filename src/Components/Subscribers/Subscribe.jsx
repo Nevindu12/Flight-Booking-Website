17f63532e23910eab09470b77f1148d54e691e20
Nevindu12 <nilangithnevindu@gmail.com>
2023-09-03 18:29:30 +0530

    add subscribe section & content

--- src/Components/Subscribers/Subscribe.jsx
+++ src/Components/Subscribers/Subscribe.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Subscribe = () => {
     return (
-        <div>Subscribe</div>
+        <div className='subscribe section'>
+            <div className="sectionContainer container">
+                <h2>Subscribe Newsletters & get Latest News</h2>
+                <div className="inputDiv flex">
+                    <input type="text" placeholder='Enter Your Email' />
+                    <button className="btn">Subscribe</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
